3bdaa759bb1480c799013664ebbe75b44e599c9a
yic95 <0Luke.Luke0@gmail.com>
2021-05-19 12:51:27 +0800

    ADD add settings
    time_format, date_format, refresh_time, date_bot_corner

--- clock.py
+++ clock.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python3
-# -*-coding: utf8-*-
+# -*- coding: utf-8 -*-
 '''
 clock
 main
@@ -22,11 +22,11 @@ class Font:
             default_font: Default font.
             font: font.
     """
-    def __init__(self, path_to_font:str=''):
+    def __init__(self, path:str=''):
         """\
         path_to_font: The path to the font file. Default is '~/.config/clock/font.json'
         """
-        self.config_font_file = path_to_font
+        self.config_path = path
         self.has_error = False
         self.font = list()
         self.default_font = [
@@ -129,20 +129,29 @@ class Font:
                 "  ###   ",
                 "  '#'   ",
             ],
+            [
+                "        ",
+                "        ",
+                "        ",
+                "        ",
+                "        ",
+                "        ",
+                "        ",
+            ],
             7  # font height
         ]
         self.font = self.default_font
-    def load_custom_font(self):
+    def load(self):
         '''\
         load custom font, save it to self.font and return True if font is valid
         if font is not valid, return False
         '''
         has_error = False
-        if isfile(self.config_font_file):
-            font = load(open(self.config_font_file))
+        if isfile(self.config_path):
+            font = load(open(self.config_path))
             has_error = False
             if bool(font) is True:
-                if type(font[11]) == type(1):
+                if type(font[-1]) == type(1):
                     self.font = font.copy()
                 else:
                     self.font = self.default_font
@@ -162,14 +171,18 @@ class Font:
 
 
 class Theme:
-    def __init__(self, path_to_theme: str=''):
-        self.config_theme_path = path_to_theme
+    """\
+    theme to change fg, bg
+    """
+    def __init__(self, path: str=''):
+        self.config_path = path
         self.default_theme = {
                 'effect':'[38;5;255;48;5;0m'}
-        self.theme=self.default_theme.copy()
-    def read_config(self):
-        if isfile(self.config_theme_path):
-            self.theme = load(open(self.config_theme_path))
+        self.theme = self.default_theme.copy()
+    def load(self):
+        """Load the theme, use default theme if custom theme isn't fcound"""
+        if isfile(self.config_path):
+            self.theme = load(open(self.config_path))
             # add default settings.
             for k, v in self.default_theme.items():
                 self.theme.setdefault(k, v)
@@ -192,10 +205,14 @@ class Settings:
         self.default_setting = {
             'font' : self.config_files_dir + '{sep}font.json'.format(sep=path_sep),
             'theme' : self.config_files_dir + '{sep}theme.json'.format(sep=path_sep),
-            'once': False
+            'once': False,
+            'date_left_bot_corner': True,
+            'refresh_time': 1,
+            'time_format': '%H:%M:%S',
+            'date_format': '%Y-%m-%d',
         }
         self.settings = self.default_setting.copy()
-    def read_settings(self)-> dict:
+    def load(self)-> dict:
         """read settings(dict) from file config_file_path"""
         if isfile(self.config_file_path):
             self.settings = load(open(self.config_file_path))
@@ -213,12 +230,11 @@ class App:
     """
     def __init__(self):
         self.settings = Settings()
-        self.settings.read_settings()
+        self.settings.load()
         self.font = Font(self.settings.settings['font'])
-        self.font.load_custom_font()
+        self.font.load()
         self.theme = Theme(self.settings.settings['theme'])
-        self.theme.read_config()
-        self.time_string = time.strftime('%H:%M:%S')
+        self.theme.load()
 
     def string_to_banner(self)->str:
         '''\
@@ -227,14 +243,16 @@ class App:
         '''
         return_text = ''
         font = self.font.font
-        string = self.time_string
-        for i in range(font[11]):
+        string = time.strftime(self.settings.settings['time_format'])
+        for i in range(font[-1]):
             for st in string:
                 if st in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'] :
                     return_text += font[int(st)][i]
-                else:
+                elif st == ':':
                     return_text += font[10][i]
-            if i != font[11]-1:  # detect if not last line
+                elif st == ' ':
+                    return_text += font[11][i]
+            if i != font[-1]-1:  # detect if not last line
                 return_text += '\n'
         return return_text
 
@@ -250,8 +268,10 @@ class App:
         banner = self.string_to_banner().split('\n')
         for i in range(len(banner)):
             banner[i] = list(banner[i])
-        banner.insert(0, list('\n'))
-        banner.insert(0, list(time.strftime('%Y-%m-%d')))
+
+        if self.settings.settings['date_left_bot_corner'] is False:
+            banner.insert(0, list('\n'))
+            banner.insert(0, list(time.strftime(self.settings.settings['date_format'])))
 
         font_height = len(banner)
 
@@ -272,8 +292,8 @@ class App:
         # insert blank lines after text
         for _ in range(int((terminal_height-font_height)/2)-1):
             banner.append(list(' '* terminal_width + '\n'))
-        banner[0].insert(0, '\033'+self.theme.theme['effect'])
-        banner[-1].append('\033[0m')
+        # banner[0].insert(0, '\033'+self.theme.theme['effect'])
+        # banner[-1].append('\033[0m')
 
         # 組合
         for i in range(len(banner)):
@@ -286,19 +306,22 @@ class App:
             return_string += '\n'
 
         # add date
-        # return_string += time.strftime('%Y-%m-%d')+'\033[0m'
+        if self.settings.settings['date_left_bot_corner'] is True:
+            return_string += ' ' + time.strftime(self.settings.settings['date_format'])+'\033[0m'
 
         # \r to return cursor to the begin of the line
-        return_string += '\r'
-        return return_string
+        return self.apply_theme(return_string)
+    def apply_theme(self, screen_text):
+        """apply theme to screen"""
+        return '\033' + self.theme.theme['effect'] + screen_text + '\033[0m'
     def run(self):
-        """run the app and exit when KeyboardInterrupt occue"""
+        """run the app and until KeyboardInterrupt occue"""
+        refresh_time = int(self.settings.settings['refresh_time'])
         if self.settings.settings['once'] == False:
             while True:
                 try:
-                    self.time_string = time.strftime('%H:%M:%S')
-                    print(self.get_screen(), end='')
-                    time.sleep(0.5)
+                    print(self.get_screen(), end='\r')
+                    time.sleep(refresh_time)
                     print('\x1b[2K\x1b[1A'*(get_terminal_size()[1]), end='')  # clear the screen
                 except KeyboardInterrupt:
                     break
